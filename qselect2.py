bdf840eebf96ab50ab23401c89481a9e9a459797
Raymond <Raymond@10-248-200-6.wireless.oregonstate.edu>
2019-09-29 16:18:53 -0700

    report.text+qselect.py change

--- qselect2.py
+++ qselect2.py
@@ -1,21 +1,24 @@
 # def qselect(num,array):
 def qselect(num,array):
-	if len(array)==num:
-		print("finished")
+	if array==[]:
 		return array
+	pivot=array[0]
+	# print(pivot)
+	left=[x for x in array if x<pivot]
+	# print(len(left))
+	if num<len(left):
+		return qselect(num,left)
+	if num==len(left)+1:
+		return pivot
 	else:
-		print(array[0])
-		pivot=array[0]
-		left=[x for x in array if x<pivot]
-		# print(pivot)
-		# print(len(array)/2)
-		if len(left)>num:
-			return qselect(num,left)+[pivot]
-		if len(left)==num-1:
-			return left[0]
-		else:
-			right=[x for x in array[1:] if x>=pivot]
-			return qselect(num,right)
+		right=[x for x in array if x>pivot]
+		# print(len(right))
+		# print(right)
+		# print("////")
+		# print(num-len(left)-1)
+		# print("====")
+
+		return qselect(num-len(left)-1,right)
 		# array=left+[pivot]+right
 		# return[x for x in array if len([y for y in array if y<=x])==num]
 		# return qselect(num,left)+[pivot]+qselect(num,right)
@@ -26,7 +29,7 @@ def qselect(num,array):
 # 	else:
 # 		return(sort(array)[num-1])
 # qselect(2, [3, 10, 4, 7, 19])
-# qselect(4, [11, 2, 8, 3])
+print(qselect(4, [11, 2, 8, 3]))
 # qselect(2,[3,10,4,7,19])
-print(qselect(2,[3,10,4,7,19]))
-# print(qselect(4, [11, 2, 8, 3]))
+# print(qselect(2,[3,10,4,7,19]))
+# print(qselect(2, [2, 2, 8, 3]))
